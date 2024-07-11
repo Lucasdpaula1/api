b71f8184e5dd65990dcaf7475b79eb1821f65e2e
lucas <lucassilvadpaula@gmail.com>
2024-07-11 12:01:56 -0300

    inicio-api

---
+++ api.js
@@ -0,0 +1,10 @@
+class formula1 {
+    constructor(name,marca){
+        this.name = name
+        this.marca = marca
+    }
+    name = ''
+    marca = ''
+}
+const jogador1 = new formula1('lucas','ferrari')
+console.log(jogador1)
